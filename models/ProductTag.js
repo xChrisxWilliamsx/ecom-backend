0ec0e90bf4480ba5f5241ed565ca6ca533c20b7a
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-04 17:50:18 -0700

    Corrects schema for productTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,12 +6,6 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
